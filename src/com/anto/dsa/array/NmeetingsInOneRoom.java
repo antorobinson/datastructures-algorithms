ab57276d292e4e7380e89c457cbc76fdc402c466
Sahaya Anto Robinson <anto_robinson@outlook.com>
2022-07-03 22:50:20 +0530

    added n-meetings-in-one-room problem

---
+++ src/com/anto/dsa/array/NmeetingsInOneRoom.java
@@ -0,0 +1,67 @@
+package com.anto.dsa.array;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * see <a href=
+ * "https://takeuforward.org/data-structure/n-meetings-in-one-room/">n-meetings-in-one-room<a/>
+ * 
+ * @author Sahaya Anto Robinson
+ * 
+ */
+public class NmeetingsInOneRoom {
+    public static void main(String args[]) {
+        int n = 6;
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 5, 7, 9, 9 };
+        maxMeetings(start, end, n);
+
+    }
+
+    public static void maxMeetings(int[] start, int[] end, int n) {
+
+        List<Meeting> meetings = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            meetings.add(new Meeting(start[i], end[i], i + 1));
+        }
+
+        meetings.sort(Comparator.comparing(Meeting::getEnd).thenComparing(Meeting::getPos));
+
+        List<Integer> result = new ArrayList<>();
+        int limit = -1;
+
+        for (Meeting m : meetings) {
+            if (m.start > limit) {
+                result.add(m.pos);
+                limit = m.end;
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
+
+class Meeting {
+    int start;
+    int end;
+    int pos;
+
+    public Meeting(int start, int end, int pos) {
+        this.start = start;
+        this.end = end;
+        this.pos = pos;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+}
